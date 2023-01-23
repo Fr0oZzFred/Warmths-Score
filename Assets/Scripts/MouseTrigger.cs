c32467566f83177c6226a42fb624d4e6f4141c25
Frédéric Deiber <frederic.deiber@gmail.com>
2023-01-23 03:40:42 +0100

    V1.1

--- Assets/Scripts/MouseTrigger.cs
+++ Assets/Scripts/MouseTrigger.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class MouseTrigger : MonoBehaviour
 {
     [SerializeField] UnityEvent Event;
+    [SerializeField] List<int> acceptedItems;
     private void OnMouseDown() {
         if (GameManager.Instance.CurrentGameState != GameManager.GameStates.InGame) return;
+        bool found = false;
+        foreach (var idx in acceptedItems) {
+            if(GameManager.Instance.Player.WearingItem == idx) {
+                found = true;
+            }
+        }
+        if (!found) return;
         Event.Invoke();
         Cursor.SetCursor(GameManager.Instance.NormalCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
